5c7f68f039115d09c88169cc746df04acc726a09
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-07-03 13:26:40 +0300

    chore first commit

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+import { PlusCircle } from "lucide-react";
+
+export const navItems = [
+    { name:"Adaugă baltă", link: "#about"},
+    { name: "Top locații", link: "#testimonials"},
+    { name: "Caută", link: "#locuri"},
+  ];
