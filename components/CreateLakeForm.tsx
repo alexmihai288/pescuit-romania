50f1c048576a9f7603f0a450038b7128141c8cb0
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-07-04 21:02:45 +0300

    chore -inputs

--- components/CreateLakeForm.tsx
+++ components/CreateLakeForm.tsx
@@ -15,17 +15,22 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { MdDriveFileRenameOutline } from "react-icons/md";
+import { MapPin } from "lucide-react";
 
 const formSchema = z.object({
-  nume_baltă: z.string().min(2, {
-    message: "Numele bății trebuie să conțină cel putin 2 caractere.",
+  nume_balta: z.string().min(2, {
+    message: "Numele bălții trebuie să conțină cel putin 2 caractere.",
+  }),
+  adresa: z.string().min(3, {
+    message: "Locația trebuie să conțină mai mult de 2 caractere",
   }),
 });
 export const CreateLakeForm = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      nume_baltă: "",
+      nume_balta: "",
+      adresa: "",
     },
   });
 
@@ -41,7 +46,7 @@ export const CreateLakeForm = () => {
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
             control={form.control}
-            name="nume_baltă"
+            name="nume_balta"
             render={({ field }) => (
               <FormItem>
                 <FormLabel className="flex items-center gap-2.5 font-bold">
@@ -58,9 +63,39 @@ export const CreateLakeForm = () => {
               </FormItem>
             )}
           />
+          <FormField
+            control={form.control}
+            name="adresa"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="flex items-center gap-2.5 font-bold">
+                  <MapPin className="w-[14px] h-[14px]" />
+                  Adresa
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Adresa bălții. Dacă balta nu are o adresă, introduceți localitatea din care face parte"
+                    {...field}
+                  />
+                  {/* <iframe
+                    title="location"
+                    className="w-full rounded-[10px] h-[250px] max-h-[250px]"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2763.020206982105!2d21.348446376218227!3d46.170253471094355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47459945cd214b5f%3A0xff0d372a84e6cae8!2sDum%20Dance%20Team%20-%20Scoala%20de%20dans!5e0!3m2!1sro!2sro!4v1697994662608!5m2!1sro!2sro"
+                    style={{ border: "0" }}
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                  ></iframe> */}
+                </FormControl>
+                <FormDescription></FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <Button type="submit">Submit</Button>
         </form>
       </Form>{" "}
     </div>
   );
 };
+
+//                 <MapPin className="w-[14px] h-[14px]" />
