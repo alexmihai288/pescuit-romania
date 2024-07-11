8c22464c671cde1347ab8ce0400e69815fc0cf11
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-07-11 13:48:31 +0300

    chore - carousel

--- app/lakes/[lakeName]/page.tsx
+++ app/lakes/[lakeName]/page.tsx
@@ -1,7 +1,15 @@
-import { Hero } from "@/components/Hero";
 import { db } from "@/lib/db";
 import React from "react";
 import { LakeHero } from "../_components/LakeHero";
+import { CgDetailsMore } from "react-icons/cg";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
 
 const LakePage = async ({ params }: { params: { lakeName: string } }) => {
   const lake = await db.lake.findFirst({
@@ -11,13 +19,39 @@ const LakePage = async ({ params }: { params: { lakeName: string } }) => {
   });
 
   return (
-    <main>
+    <main className="bg-[#f3f4f6]">
       <div>
-      <LakeHero lake={lake!}/>
+        <LakeHero lake={lake!} />
       </div>
-      <div className="my-32 container bg-[#f3f4f6]">
-        daskodakso
+      <div className="container my-32">
+        <div className="flex justify-between bg-white rounded-md">
+          <div className="p-2.5">
+            <h1 className="font-extrabold flex items-center gap-2">
+              <CgDetailsMore /> Detalii
+            </h1>
+            <p className="text-sm">{lake?.lakeInfo}</p>
+          </div>
+          <Carousel>
+            <CarouselContent className="max-w-3xl">
+              {lake?.galleryImageUrls.map((galleryImg) => (
+                <CarouselItem key={galleryImg}>
+                  <Image
+                    src={galleryImg}
+                    width={800}
+                    height={800}
+                    alt="gallery image"
+                    className="rounded-r-md"
+                  />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
       </div>
+
+      <div>test</div>
     </main>
   );
 };
