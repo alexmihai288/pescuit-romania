041052f33450c15ac7ee7b583206e6498ea5326b
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-07-13 13:19:35 +0300

    chore - facilities style

--- app/adauga-balta/_components/AddFacilities.tsx
+++ app/adauga-balta/_components/AddFacilities.tsx
@@ -1,6 +1,48 @@
+import { Separator } from "@/components/ui/separator";
 import { Facilities } from "@prisma/client";
-import React from "react";
+import { Check, Plus } from "lucide-react";
+import React, { useState } from "react";
+
+const facilitiesMapping = {
+  PlataCard: "Plata Card",
+  AccesMasina: "Acces Mașină",
+  Casute: "Căsuțe",
+  Gratar: "Grătar",
+  Iluminat: "Iluminat",
+  InchirieriBarci: "Închirieri Bărci",
+  Internet: "Internet",
+  LocPentruCopii: "Loc Pentru Copii",
+  MagazinMomeala: "Magazin Momeală",
+  Pontoane: "Pontoane",
+  Toalete: "Toalete",
+  Umbra: "Umbră",
+};
 
 export const AddFacilities = () => {
-  return <div className="flex items-center justify-between">{Facilities.AccesMasina}</div>;
+  const facilitiesArray = Object.values(Facilities);
+
+  const [addedFacilities, setAddedFacilities] = useState<string[]>([]);
+
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-2.5 justify-center">
+        {facilitiesArray.map((facility, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-2.5 bg-zinc-300 w-fit rounded-full p-2.5 hover:bg-zinc-500 transition-colors cursor-pointer"
+          >
+            {facilitiesMapping[facility]}
+            <Plus className="w-[15px] h-[15px] text-zinc-600" />
+          </div>
+        ))}
+      </div>
+      <Separator orientation="horizontal"/>
+      <div className="flex items-center gap-2.5">
+        <div className="flex items-center gap-2.5 bg-green-300 w-fit rounded-full p-2.5 hover:bg-green-500 transition-colors cursor-pointer">
+          {facilitiesMapping.AccesMasina}
+          <Check className="w-[15px] h-[15px] text-green-600" />
+        </div>
+      </div>
+    </>
+  );
 };
